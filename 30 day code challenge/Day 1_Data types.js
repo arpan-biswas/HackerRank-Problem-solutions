5e64115925f7610d10b4d179fe9738cba3f08d16
abc <ninetyskid.abc@gmail.com>
2019-12-10 16:39:19 +0600

    code modified and bug fixed

--- 30 day code challenge/Day 1_Data types.js
+++ 30 day code challenge/Day 1_Data types.js
@@ -24,20 +24,23 @@ function main() {
     var d = 4.0
     var s = "HackerRank "
     // Declare second integer, double, and String variables.
-       var i, d, s;
+        var x, y, z;
     // Read and save an integer, double, and String to your variables.
     
-        var x= 12;
-        var y= 4.0;
-        var z = "is the best place to learn and practice coding!";
+        x =parseInt(readLine());
+        x= x+i;
+        y= parseFloat(readLine());
+        y= y+d;
+        z= s + readLine();
+               
+
+
     // Print the sum of both integer variables on a new line.
-         x= x + i;
          console.log(x) ;
         
     // Print the sum of the double variables on a new line.
-        sum = (y + d);
-        console.log((sum).toFixed(1));
+        console.log(y.toFixed(1));
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
-    console.log(s + z);
+    console.log( z);
 }
