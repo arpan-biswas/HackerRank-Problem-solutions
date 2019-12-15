bbfc0ec372ee737aac9abe8fea05d99be8ff7780
unknown <ninetyskid.abc@gmail.com>
2019-12-16 00:47:26 +0600

    sql problems are solved

---
+++ Sql/--Weather Observation Station 6.sql
@@ -0,0 +1,11 @@
+--Weather Observation Station 6
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+
+
+/*
+Enter your query here.
+*/
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[aeiou]';
